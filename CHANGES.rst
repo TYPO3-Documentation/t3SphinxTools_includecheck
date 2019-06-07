b1d107728a9df5e0d47c01b2b75c8c1722f22986
Martin Bless <martin.bless@mbless.de>
2019-06-07 11:09:10 +0200

    v0.2.2 (Released July 26, 2013)

---
+++ CHANGES.rst
@@ -0,0 +1,11 @@
+
+Release v0.2.2 (July 26, 2013)
+==============================
+
+v0.2.2
+
+* Legacy version from
+  https://raw.githubusercontent.com/TYPO3-Documentation/typo3-docs-typo3-org-resources/master/userroot/scripts/bin/check_include_files.py
+  Has been in use since then (as of 2019-06-07).
+
+End - beginning of time.
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='t3sphinxtools_includecheck',
+      version='1.0',
+      description='Check file paths of included reST files',
+      url='http://github.com/TYPO3-Documentation/t3SphinxTools_checkincludes',
+      author='Martin Bless',
+      author_email='martin.bless@mbless.de',
+      license='MIT',
+      packages=['t3sphinxtools_includecheck'],
+      zip_safe=False)
